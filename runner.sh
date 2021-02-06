0ffb20cad9a794de56f529ca86db399a2342f1ea
Nimish Bongale <nimishnb98@gmail.com>
2021-02-06 21:19:05 +0530

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -2,5 +2,5 @@ pip3 install virtualenv --user
 /root/.local/bin/virtualenv venv -p python3
 . .venv/bin/activate
 pip3 install -r requirements.txt
-chmod +x engines/stockfish
+chmod +x engines/AndThenThereWereNone
 python3 lichess-bot.py
